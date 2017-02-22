0d72934c76385d1f004f60d3156d3be9ff8849ae
Jing Chen <chenjing412@users.noreply.github.com>
2017-02-22 15:09:14 -0500

    Add files via upload

---
+++ aggF.py
@@ -0,0 +1,29 @@
+# Function: Aggregation   
+# Version: 1.0
+# Date: Feb 22, 2017
+# Author: Jing Chen
+
+
+def aggF (r):
+#
+#Input variable:
+# r: the row aggregation scheme. 
+#Output variable:
+#S: the aggregation matrix given by the aggregation scheme r.
+# Reference: Stairs (2013): An Aggregation Matrix MatLab Function.
+    import numpy as np
+    row=r
+    col=np.array(range(len(r)))
+    data=np.array([1]*len(r))
+    from scipy.sparse import coo_matrix
+    s = coo_matrix((data, (row, col)), shape=(max(r)+1, len(r))).toarray()
+    # start from zero
+    return (s)
+
+
+
+
+
+
+
+
